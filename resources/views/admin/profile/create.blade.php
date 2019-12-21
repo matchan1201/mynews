2af1766a26dccfe9e1944bc111246cd9f388177e
”matchan” <bumpakano.1@gmail.com>
2019-12-21 05:36:57 +0000

    laravel part3

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, intial-scale=1">
+        
+        <title>プログラミング勉強日記</title>
+    </head>
+    <body>
+        <h1>今日のスケジュール</h1>
+    </body>
+</html>
