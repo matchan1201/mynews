cd75bee4b88703a42c3e9f4c6518ce73a95a518c
”matchan” <bumpakano.1@gmail.com>
2019-12-23 10:10:32 +0000

    課題laravel13章

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
